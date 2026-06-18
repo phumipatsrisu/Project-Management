f405c7714e21f6961b843636801c3f9ea7486bb1
Phumipat <158813193+phumipatsrisu@users.noreply.github.com>
2026-06-18 17:51:59 +0700

    Improve authorization error handling in authCheck middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,9 +2,12 @@ const jwt = require("jsonwebtoken");
 
 exports.authCheck = async (req, res, next) => {
   try {
+    if (!req.headers.authorization) {
+      return res.status(401).json({ message: "Authorization header missing" });
+    }
     const token = req.headers.authorization.replace("Bearer ", "");
     if (!token) {
-      return res.status(401).json({ message: "Don't have token" });
+      return res.status(401).json({ message: "No token provided" });
     }
 
     const decode = jwt.verify(token, process.env.JWT_SECRET);
@@ -12,6 +15,6 @@ exports.authCheck = async (req, res, next) => {
     next();
   } catch (error) {
     console.log(error);
-    res.status(401).json({ message: "Token Invalid" });
+    res.status(401).json({ message: "Token invalid" });
   }
 };
